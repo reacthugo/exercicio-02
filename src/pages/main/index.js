f0b70c202367f48b33b2ba713cbb7619104af31e
HugoVinicius <hugo.desenvolvimento@outlook.com>
2019-08-25 22:33:22 -0300

    first commit

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -5,6 +5,7 @@ import './styles.css';
 
 const Main = () => (
     <div className="home">
+        <h1>Olá dev, seja muito bem vindo!</h1>
         <Link className="actions" to="/users">Listar usuários</Link>
     </div >
 );
